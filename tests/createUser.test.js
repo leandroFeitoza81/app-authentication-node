ead2376df638c69d410c5f2acfb72f3a81c7e116
Leandro <leandro.feitoza@gmail.com>
2021-09-13 20:36:59 -0300

    antes da implementação do hash de senha

--- tests/createUser.test.js
+++ tests/createUser.test.js
@@ -2,14 +2,10 @@ const request = require('supertest');
 const app = require('../src/app');
 const { expect } = require('chai');
 const truncate = require('./utils/truncate');
-const shell = require('shelljs');
 
 describe('Test the route [POST] "/api/users/register"', () => {
-  beforeEach(() => {
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:drop');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:create');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:migrate');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:seed:all');
+  beforeEach(async () => {
+    await truncate();
   });
 
   it('Test it if is possible register a user', async () => {
@@ -24,6 +20,7 @@ describe('Test the route [POST] "/api/users/register"', () => {
     expect(response.statusCode).to.equal(201);
     expect(response.body).to.be.a('object');
     expect(response.body).to.be.include({
+      id: 1,
       name: 'Tom',
       last_name: 'York',
       username: 't.york',
--- tests/getAllUsers.test.js
+++ tests/getAllUsers.test.js
@@ -1,26 +1,24 @@
 const request = require('supertest');
 const app = require('../src/app');
 const { expect } = require('chai');
-const shell = require('shelljs');
+
 const { users } = require('../src/models');
 
 describe('Test the route [GET] -> "/api/users"', () => {
-  beforeEach(() => {
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:drop');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:create');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:migrate');
-    shell.exec('NODE_ENV=test yarn sequelize-cli db:seed:all');
-  });
-
   it('Test it if returns an array of users', async () => {
+    const data = [
+      {
+        id: 1,
+        name: 'Ian',
+        last_name: 'Curtis',
+        username: 'i.curtis',
+        password: 'senha123',
+      },
+    ];
+    await users.create(data[0]);
     const response = await request(app).get('/api/users');
     expect(response.statusCode).to.equal(200);
-    expect(response.body).to.have.lengthOf(3);
-    expect(response.body[0]).to.include({
-      name: 'Chris',
-      last_name: 'Cornell',
-      username: 'c.cornell',
-      password: 'senha123',
-    });
+    expect(response.body).to.have.lengthOf(1);
+    expect(response.body).to.deep.equal(data);
   });
 });
